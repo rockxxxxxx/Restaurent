54f1d62696885bf331e1eadf1f04d9fbe6b6448d
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-09-26 15:23:15 +0530

    Added Restaurant Summary

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -14,8 +14,19 @@ export default function Navbar() {
           &nbsp;&nbsp;Your Cart <span className="count">0</span>
         </p>
       </div>
-      <div className="mealsImage">
+      <div className="MealsImage">
         <img className="mealsImage" src="meals.jpg" alt="" />
+        <span className="card">
+          <h1>Delecious Food, Delevired to you</h1>
+          <p>
+            Choose your favourite meals from our brand selection of available
+            meals and enjoy a delecious lunch or dinner at home.
+          </p>
+          <p>
+            All our meals are cooked with high-quality ingredients, just-in-time
+            and ofcourse by experienced chefs!
+          </p>
+        </span>
       </div>
     </>
   );
